8c2ed1713e895fb5e6aabb76c4513c00d4416687
natwar.patidar <natwar.patidar@gammaedge.io>
2025-09-23 15:41:59 -0400

    Enhance UI

--- src/components/heroCard/HeroCard.jsx
+++ src/components/heroCard/HeroCard.jsx
@@ -8,15 +8,21 @@ export default function HeroCard({ locationString }) {
 
     const [city, setCity] = useState("");
     const [weatherData, setWeatherData] = useState(null);
+    const [fetching, setFetching] = useState(false);
+    const [error, setError] = useState(false);
     
     const handleSubmit = async(e) => {
         e.preventDefault();
+        setFetching(true)
         if (!city.trim()) return; 
         try {
             const res = await getForecastData(city);
             setWeatherData(res);
         } catch (error) {
             console.log(error);
+            setError(error);
+        } finally {
+            setFetching(false);
         }
     };
 
@@ -60,17 +66,19 @@ export default function HeroCard({ locationString }) {
                 <div className="rounded-3xl h-full shadow-2xl drop-shadow-2xl flex items-center justify-center md:p-8 lg:p-10" style={{boxShadow: '0 25px 50px -12px rgba(0, 0, 0, 0.25), 0 0 0 1px rgba(255, 255, 255, 0.1)'}}>
                 
                     <div className="flex flex-col sm:flex-row items-center justify-center gap-12 mb-4 px-64">
-                        {
+                        { error && (<p className="text-red-500">{error}</p>) }
+                        { fetching && <p className="text-black/35">Loading...</p> }
+                        { 
                             weatherData && (
                                 <>
                                     <div className="flex-shrink-0">
-                                        <img src={weatherData?.current?.condition?.icon} className="h-16 w-16 md:h-20 md:w-20 text-gray-600" />
+                                        <img src={weatherData?.current?.condition?.icon} className="w-[250px]" />
                                     </div>
                                     <div className="text-center sm:text-left">
-                                        <div className="text-sm md:text-base text-gray-600">Today</div>
-                                        <div className="text-3xl font-semibold md:text-4xl text-gray-700 mb-1">{weatherData?.location?.name}</div>
-                                        <div className="text-sm md:text-base text-gray-600">Temperature: {weatherData?.current?.temp_c}&deg;C</div>
-                                        <div className="text-sm md:text-base text-gray-500 font-medium">{weatherData?.current?.condition?.text}</div>
+                                        <div className="text-sm md:text-base text-gray-700">Today</div>
+                                        <div className="text-3xl font-semibold md:text-4xl mb-1">{weatherData?.location?.name}</div>
+                                        <div className="text-sm md:text-base text-gray-700">Temperature: {weatherData?.current?.temp_c}&deg;C</div>
+                                        <div className="text-sm md:text-base text-gray-700 font-medium">{weatherData?.current?.condition?.text}</div>
                                     </div>
                                 </>
                             )
